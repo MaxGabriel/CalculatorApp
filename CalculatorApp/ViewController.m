c6021f1c5a243050598ba706f14cfd3fe6f0ae37
Maximilian Tagher <feedback.tagher@gmail.com>
2014-04-02 22:16:02 -0700

    Initial commit

--- CalculatorApp/ViewController.m
+++ CalculatorApp/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UITextField *mathField;
+@property (strong, nonatomic) IBOutlet UIButton *solveButton;
+@property (strong, nonatomic) IBOutlet UILabel *resultField;
+
+
 @end
 
 @implementation ViewController
@@ -20,10 +25,14 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)solveExpression:(UIButton *)sender {
+    @try {
+        NSNumber *result = [[NSExpression expressionWithFormat:self.mathField.text,nil] expressionValueWithObject:nil context:nil];
+        self.resultField.text = [NSString stringWithFormat:@"%@",result];
+    }
+    @catch (NSException *exception) {
+        [[[UIAlertView alloc] initWithTitle:@"Error" message:@"Bad expression" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil] show];
+    }
 }
 
 @end
